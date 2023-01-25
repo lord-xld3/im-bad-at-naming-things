3f8174b0dde54c8745f0f71930395278714092c7
Kyle Simmons <simmonskyleb@gmail.com>
2023-01-25 14:07:41 -0700

    looks even better, still does nothing

--- build/script.js
+++ build/script.js
@@ -1,16 +1,16 @@
 "use strict";
 var currentDate = new Date(); // display for user, everything else will be in unix time for precise timers
-var currentDateKey = currentDate.toDateString();
 var currentHour = currentDate.getHours();
-$("#currentDay").append(currentDateKey);
-$(() => {
+$(function initPage() {
+    var currentDateKey = currentDate.toDateString();
+    $("#currentDay").text(currentDateKey);
     for (var t = 0; t < 24; t++) {
         // Determine blockColor
         let blockColor = (t < currentHour) ? "past"
             : (t == currentHour) ? "present"
                 : "future";
         // Create & append block to page
-        let createBlock = $("<div id='" + t + "' class='row time-block'><div class='col-2 col-md-1 hour text-center py-3'>" + t + ":00</div><div class='col description " + blockColor + "'></div></div>");
+        let createBlock = $("<div id='" + t + "' class='row time-block'><div class='col-2 hour text-center py-3'>" + t + ":00</div><div class='col description " + blockColor + "'></div></div>");
         $("#eventContainer").append(createBlock);
     }
     if (localStorage.getItem("recurEvents") != null)
--- src/script.ts
+++ src/script.ts
@@ -1,9 +1,11 @@
 var currentDate = new Date() // display for user, everything else will be in unix time for precise timers
-var currentDateKey = currentDate.toDateString()
 var currentHour = currentDate.getHours()
-$("#currentDay").append(currentDateKey)
 
-$(()=>{
+$(function initPage(){
+	
+	var currentDateKey = currentDate.toDateString()
+	$("#currentDay").text(currentDateKey)
+	
 	for (var t=0; t < 24; t++){
 		// Determine blockColor
 		let blockColor = (t < currentHour)?"past"
@@ -11,7 +13,7 @@ $(()=>{
 		:"future"
 		
 		// Create & append block to page
-		let createBlock = $("<div id='" + t + "' class='row time-block'><div class='col-2 col-md-1 hour text-center py-3'>" + t + ":00</div><div class='col description " + blockColor + "'></div></div>")
+		let createBlock = $("<div id='" + t + "' class='row time-block'><div class='col-2 hour text-center py-3'>" + t + ":00</div><div class='col description " + blockColor + "'></div></div>")
 		$("#eventContainer").append(createBlock)
 	}
 
