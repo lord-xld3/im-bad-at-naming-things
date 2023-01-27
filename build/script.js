c9a2c845071e16a60eea83932461c6531f932c1f
Kyle Simmons <simmonskyleb@gmail.com>
2023-01-27 05:11:46 -0700

    ready to define/save data

--- build/script.js
+++ build/script.js
@@ -1,23 +1,56 @@
 "use strict";
-var currentDate = new Date(); // display for user, everything else will be in unix time for precise timers
+var currentDate = new Date();
 var currentHour = currentDate.getHours();
+var currentDateKey = currentDate.toDateString();
+$("#currentDay").text(currentDateKey);
 $("#createEvent").on("click", () => {
     $("#modalCreate").css("display", "block");
+});
+$("#showRepeat").on("click", () => {
+    if (($("#repeatOptions").css("display")) == "none") {
+        $("#repeatOptions").css("display", "block");
+    }
+    else
+        $("#repeatOptions").css("display", "none");
+});
+$("#saveBtn").on("click", () => {
+    if ($("#eventStart").val() == ""
+        || $("#eventEnd").val() == ""
+        || $("#eventText").val() == "") {
+        return alert("Please fill out required fields");
+    }
+    else {
+        let weekArr = $(".weekNum").toArray();
+        let dayArr = $(".weekDay").toArray();
+        if (weekArr.every((e) => e.checked == false)
+            && dayArr.some((e) => e.checked == true)) {
+            alert("When selecting a week, you must also select a day");
+        }
+        else if (weekArr.some((e) => e.checked == true)
+            && dayArr.every((e) => e.checked == false)) {
+            alert("When selecting a day, you must also select a week");
+        }
+        else {
+            let currentDateObj = localStorage.getItem(currentDateKey);
+            if (currentDateObj == null) {
+                currentDateObj = { eventArray: [] };
+            }
+            let event = {
+                eventStart: ($("#eventStart"))
+            };
+        }
+    }
 });
 $(".close").on("click", () => {
     $(".close").parent().parent().css("display", "none");
 });
 $(function initPage() {
-    var currentDateKey = currentDate.toDateString(); //shorten date
-    $("#currentDay").text(currentDateKey); // display it
     for (let t = 0; t < 24; t++) {
-        // Determine blockColor
         let blockColor = (t < currentHour) ? "past"
             : (t == currentHour) ? "present"
                 : "future";
         if (t < 10)
-            t = "0" + t; // use 00:00 instead of 0:00
-        // Create & append block to page
+            t = "0" + t;
         let createBlock = $("<div id='" + t + "' class='row time-block'><div class='col-2 hour text-center py-3'>" + t + ":00</div><div class='col description " + blockColor + "'></div></div>");
         $("#eventContainer").append(createBlock);
     }
--- src/script.ts
+++ src/script.ts
@@ -1,18 +1,78 @@
+//#region Init
 var currentDate = new Date() // display for user, everything else will be in unix time for precise timers
 var currentHour = currentDate.getHours()
+var currentDateKey = currentDate.toDateString() //shorten date
+	$("#currentDay").text(currentDateKey) // ^ display it
+
+// Open 'create' modal
 $("#createEvent").on("click",()=>{
 	$("#modalCreate").css("display","block")
 })
 
+// Show/Hide "Repeat?" options
+$("#showRepeat").on("click",()=>{
+	if (($("#repeatOptions").css("display"))=="none"){
+		$("#repeatOptions").css("display","block")
+	}
+	else $("#repeatOptions").css("display","none")
+})
+
+// Save event
+$("#saveBtn").on("click",()=>{
+	
+	// If required fields are blank...
+	if ($("#eventStart").val()==""
+	||$("#eventEnd").val()==""
+	||$("#eventText").val()==""){
+		return alert("Please fill out required fields")
+	}
+
+	else{ // Parse input data, still handle invalid input
+
+		let weekArr = $(".weekNum").toArray()
+		let dayArr = $(".weekDay").toArray()
+
+	//#region Brain-pain zone!!!
+		
+		// If no week is checked, return "true"
+		if (weekArr.every((e:any)=>e.checked==false) 
+		
+		// If any day is checked, return "true"
+		&& dayArr.some((e:any)=>e.checked==true)){
+			
+			// No week is checked & some day is checked
+			alert("When selecting a week, you must also select a day")
+		}
+
+		// If some week is checked, return "true"
+		else if (weekArr.some((e:any)=>e.checked==true)
+
+		// If every day is unchecked, return "true"
+		&& dayArr.every((e:any)=>e.checked==false)){
+
+			// Some day is checked & no week is checked
+			alert("When selecting a day, you must also select a week")
+		}
+	//#endregion
+
+		else {
+			let currentDateObj:string|object|null = localStorage.getItem(currentDateKey)
+			if (currentDateObj==null){
+				currentDateObj = {eventArray:[]}
+			}
+			let event = {
+				eventStart: ($("#eventStart"))
+			}
+		}
+	}
+})
+
+// Close any modal
 $(".close").on("click",()=>{
 	$(".close").parent().parent().css("display", "none")
 })
 
 $(function initPage(){
-	
-	var currentDateKey = currentDate.toDateString() //shorten date
-	$("#currentDay").text(currentDateKey) // display it
-	
 	for (let t:any=0; t < 24; t++){
 		// Determine blockColor
 		let blockColor = (t < currentHour)?"past"
@@ -34,7 +94,7 @@ $(function initPage(){
 	}
 	return
 })
-
+//#endregion
 function checkEventRecurrence(recurEvents){
 
 }
